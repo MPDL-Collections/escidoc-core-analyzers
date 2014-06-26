87dff36befd2d73ab7b6022eff670181b6104005
sieders <siedersleben@mpdl.mpg.de>
2014-06-26 12:31:28 +0200

    show differences between different Lucene Versions of StandardAnalyzer

---
+++ src/test/de/escidoc/sb/common/lucene/analyzer/TestStandardAnalyzer.java
@@ -0,0 +1,284 @@
+package test.de.escidoc.sb.common.lucene.analyzer;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.util.Version;
+import org.junit.Test;
+
+public class TestStandardAnalyzer extends TestBase {
+
+	private StandardAnalyzer analyzer;
+
+	@Test
+	public void testLuceneVersion_3_3() throws Exception {
+		
+		analyzer = new StandardAnalyzer(Version.LUCENE_33);
+		tokenizeWithTokenizer(analyzer);
+		
+		analyzer = new StandardAnalyzer(Version.LUCENE_34);
+		tokenizeWithTokenizer(analyzer);
+		
+		analyzer = new StandardAnalyzer(Version.LUCENE_30);
+		tokenizeWithTokenizer(analyzer);
+	}
+
+	private void tokenizeWithTokenizer(Analyzer analyzer) throws IOException {
+		TokenStream result;
+
+		result = analyzer.tokenStream("field", new StringReader("abc/cfg"));
+		List<String> tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("abc"));
+		assertTrue(tokens.get(1).equals("cfg"));
+		tokens.clear();
+
+		result = analyzer.tokenStream("field", new StringReader("abc,def"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("abc"));
+		assertTrue(tokens.get(1).equals("def"));
+		tokens.clear();
+		
+		result = analyzer.tokenStream("field", new StringReader("abc.def"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 1);
+		assertTrue(tokens.get(0).equals("abc.def"));
+		tokens.clear();
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"2011-01-28T15:14:30.925Z"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 5);
+		assertTrue(tokens.get(0).equals("2011"));
+		assertTrue(tokens.get(1).equals("01"));
+		assertTrue(tokens.get(2).equals("28t15"));
+		assertTrue(tokens.get(3).equals("14"));
+		assertTrue(tokens.get(4).equals("30.925z"));
+		tokens.clear();
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"hdl:11858/00-001Z-0000-0023-453D-4"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 8);
+		assertTrue(tokens.get(0).equals("hdl"));
+		assertTrue(tokens.get(1).equals("11858"));
+		assertTrue(tokens.get(2).equals("00"));
+		assertTrue(tokens.get(3).equals("001z"));
+		assertTrue(tokens.get(4).equals("0000"));
+		assertTrue(tokens.get(5).equals("0023"));
+		assertTrue(tokens.get(6).equals("453d"));
+		assertTrue(tokens.get(7).equals("4"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"http://pubman.mpdl.mpg.de/pubman"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 3);
+		assertTrue(tokens.get(0).equals("http"));
+		assertTrue(tokens.get(1).equals("pubman.mpdl.mpg.de"));
+		assertTrue(tokens.get(2).equals("pubman"));
+
+		/**
+		result = analyzer.tokenStream("field", new StringReader(
+				"http://purl.org/eprint/type/Book"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("http://purl"));
+		assertTrue(tokens.get(1).equals("org/eprint/type/book"));
+
+		result = analyzer.tokenStream("field", new StringReader("S5 0014+813"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("s5"));
+		assertTrue(tokens.get(1).equals("0014+813"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"Black Hole in Sgr A(*)"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 4);
+		assertTrue(tokens.get(0).equals("black"));
+		assertTrue(tokens.get(1).equals("hole"));
+		assertTrue(tokens.get(2).equals("sgr"));
+		assertTrue(tokens.get(3).equals("a(*"));
+
+		result = analyzer.tokenStream("field", new StringReader("abc_cdf"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 1);
+		assertTrue(tokens.get(0).equals("abc_cdf"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"(001)-oriented MgO"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("001)-oriented"));
+		assertTrue(tokens.get(1).equals("mgo"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"(001)-oriented TbBaCo2O6-α"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("001)-oriented"));
+		assertTrue(tokens.get(1).equals("tbbaco2o6-α"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"galaxy IRAS 00182−7112"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 3);
+		assertTrue(tokens.get(0).equals("galaxy"));
+		assertTrue(tokens.get(1).equals("iras"));
+		assertTrue(tokens.get(2).equals("00182−7112"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"Techniken für 0,01-mm3-Auflösung"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 4);
+		assertTrue(tokens.get(0).equals("techniken"));
+		assertTrue(tokens.get(1).equals("fur"));
+		assertTrue(tokens.get(2).equals("0"));
+		assertTrue(tokens.get(3).equals("01-mm3-auflosung"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"01-P003 The role of miRNAs"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 3);
+		assertTrue(tokens.get(0).equals("01-p003"));
+		assertTrue(tokens.get(1).equals("role"));
+		assertTrue(tokens.get(2).equals("mirnas"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"supernova remnant 1E 0102.2-7219"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 4);
+		assertTrue(tokens.get(0).equals("supernova"));
+		assertTrue(tokens.get(1).equals("remnant"));
+		assertTrue(tokens.get(2).equals("1e"));
+		assertTrue(tokens.get(3).equals("0102.2-7219"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"quasar SDSS J172206.03+565451.6"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 5);
+		assertTrue(tokens.get(0).equals("quasar"));
+		assertTrue(tokens.get(1).equals("sdss"));
+		assertTrue(tokens.get(2).equals("j172206"));
+		assertTrue(tokens.get(3).equals("03+565451"));
+		assertTrue(tokens.get(4).equals("6"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"0[110] transformation"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("0[110]"));
+		assertTrue(tokens.get(1).equals("transformation"));
+
+		result = analyzer
+				.tokenStream(
+						"field",
+						new StringReader(
+								"pi-Congested poly(paraphenylene) from 2,2',6,6'-tetraphenyl-1,1'-biphenyl units"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 9);
+		assertTrue(tokens.get(0).equals("pi-congested"));
+		assertTrue(tokens.get(1).equals("poly(paraphenylene"));
+		assertTrue(tokens.get(2).equals("from"));
+		assertTrue(tokens.get(3).equals("2"));
+		assertTrue(tokens.get(4).equals("2"));
+		assertTrue(tokens.get(5).equals("6"));
+		assertTrue(tokens.get(6).equals("6'-tetraphenyl-1"));
+		assertTrue(tokens.get(7).equals("1'-biphenyl"));
+		assertTrue(tokens.get(8).equals("units"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"1,5-Dimethoxy-9,10-di(phenylethynyl)anthracene fluorophore"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 4);
+		assertTrue(tokens.get(0).equals("1"));
+		assertTrue(tokens.get(1).equals("5-dimethoxy-9"));
+		assertTrue(tokens.get(2).equals("10-di(phenylethynyl)anthracene"));
+		assertTrue(tokens.get(3).equals("fluorophore"));
+
+		result = analyzer.tokenStream("field",
+				new StringReader("Pb-207/Pb-204"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 1);
+		assertTrue(tokens.get(0).equals("pb-207/pb-204"));
+
+		result = analyzer.tokenStream("field", new StringReader("Nb/La"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 1);
+		assertTrue(tokens.get(0).equals("nb/la"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"Sgr A*'s near-infrared"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 3);
+		assertTrue(tokens.get(0).equals("sgr"));
+		assertTrue(tokens.get(1).equals("a*'s"));
+		assertTrue(tokens.get(2).equals("near-infrared"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"1,3-Bicyclo[1.1.1]pentanediyl"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 3);
+		assertTrue(tokens.get(0).equals("1"));
+		assertTrue(tokens.get(1).equals("3-bicyclo[1"));
+		assertTrue(tokens.get(2).equals("1.1]pentanediyl"));
+
+		result = analyzer.tokenStream("field", new StringReader("Meier, Hans"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("meier"));
+		assertTrue(tokens.get(1).equals("hans"));
+
+		result = analyzer
+				.tokenStream("field", new StringReader("Xaver Müller"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("xaver"));
+		assertTrue(tokens.get(1).equals("muller"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"with other right"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("other"));
+		assertTrue(tokens.get(1).equals("right"));
+
+		result = analyzer.tokenStream("field",
+				new StringReader("God and Right"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("god"));
+		assertTrue(tokens.get(1).equals("right"));
+
+		result = analyzer.tokenStream("field", new StringReader(
+				"Gott und Recht"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 2);
+		assertTrue(tokens.get(0).equals("gott"));
+		assertTrue(tokens.get(1).equals("recht"));
+*/
+		result = analyzer.tokenStream("field", new StringReader(
+				"we never use the same brain twice"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 6);
+		assertTrue(tokens.get(0).equals("we"));
+		assertTrue(tokens.get(1).equals("never"));
+		assertTrue(tokens.get(2).equals("use"));
+		assertTrue(tokens.get(3).equals("same"));
+		assertTrue(tokens.get(4).equals("brain"));
+		assertTrue(tokens.get(5).equals("twice"));
+
+		result = analyzer.tokenStream("field", new StringReader("für"));
+		tokens = doTokenizing(result);
+		assertTrue(tokens.size() == 1);
+		
+	}
+
+}
