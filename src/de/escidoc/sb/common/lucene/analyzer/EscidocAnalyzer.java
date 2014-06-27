6297fe662b6e01fa6c2717e992818420ea4931f4
sieders <siedersleben@mpdl.mpg.de>
2014-06-27 10:11:06 +0200

    optimize imports

--- src/de/escidoc/sb/common/lucene/analyzer/EscidocAnalyzer.java
+++ src/de/escidoc/sb/common/lucene/analyzer/EscidocAnalyzer.java
@@ -40,7 +40,6 @@ import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
-import org.apache.lucene.util.Version;
 
 /**
  * Analyzer for all escidoc lucene indices.
